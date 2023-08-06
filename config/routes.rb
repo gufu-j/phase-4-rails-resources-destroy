ee0540f295bd37316be983962ab3467fd18d8978
diego <guatemaladiegojose@gmail.com>
2023-08-05 21:42:24 -0400

    added destroy method to respond delete request

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  # resources :birds, only: [:index, :show, :create, :update, :destroy]
+  #we commented because now we are using the all five RESTful routes so we can simply do
+  resources :birds
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
